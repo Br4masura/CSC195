3e220d8c6552459cf0b0483d7f0e63a98d99103c
Br4masura <blackbird100804@gmail.com>
2025-04-06 20:28:31 -0600

    Primitive Data Types Payroll Assignment

---
+++ UserDataType/UserDataType/UserDataTypes.cpp
@@ -0,0 +1,19 @@
+
+#include "Declarations.h"
+#include <iostream>
+
+//typedef double salary; traditional c++ way
+using salary = double; //modern c++ way
+//both create a context
+using Sandwich = char;
+
+
+int main() {
+
+	salary wage_reception = 14.4;
+	salary wage_analyst = 18.5;
+	salary wage_engineer = 32.4;
+	 
+	Sandwich pastrami; 
+
+}
